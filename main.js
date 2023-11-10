b638e903599bad2511af92a3e7980f9109f42b7c
LUIS 2E <105930017+Luis1B@users.noreply.github.com>
2023-11-10 10:42:43 -0300

    a

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classlist[1]
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
-        tocasom(idAudio)
+const efeito = listaDeTeclas[contador].classlist[1]
+const idAudio = "#som_"+efeito;
+listaDeTeclas[contador].onclick = function(){
+    tocasom(idAudio)
 
-    }
+}
     contador = contador +1;
     console.log(contador);
 }
