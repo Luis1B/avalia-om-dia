a114ef7a38af3827cfdf3597bf602ef8bfbf74b2
LUIS 2E <105930017+Luis1B@users.noreply.github.com>
2023-11-10 10:38:08 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("Olá segundo ano mais lindo")
+function tocasom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classlist[1]
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocasom(idAudio)
+
+    }
+    contador = contador +1;
+    console.log(contador);
+}
